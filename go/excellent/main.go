f96da56dd9e0838a0b34bce08e883e2ea11e13ab
kuron <stn.chronocs1@gmail.com>
2024-06-02 03:51:07 +0900

    add: release config

--- go/excellent/main.go
+++ go/excellent/main.go
@@ -1,9 +1,16 @@
 package main
 
+import "fmt"
+
 func EvenOrOdd(number int) string {
 	if number%2==0{
 		return "even"
 	}else{
 		return "odd"
 	}
+}
+
+var version string
+func main(){
+	fmt.Printf("Example %s\n",version)
 }
