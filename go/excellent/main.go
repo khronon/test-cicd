8bf6fc4576ffad552e0fd5c89fea7f7d3c46b96a
kuron <stn.chronocs1@gmail.com>
2024-06-02 03:57:18 +0900

    fix: file structure

--- go/excellent/main.go
+++ go/excellent/main.go
@@ -1,16 +1,9 @@
 package main
 
-import "fmt"
-
 func EvenOrOdd(number int) string {
 	if number%2==0{
 		return "even"
 	}else{
 		return "odd"
 	}
-}
-
-var version string
-func main(){
-	fmt.Printf("Example %s\n",version)
 }
